944d873dba266580051a18912fe37d453afe0e10
nikkhil <nikkhil312@gmail.com>
2022-11-01 01:54:23 +0530

    new way added for NO button

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const b=document.querySelector('button.no')
-b.addEventListener('mouseover',()=>{
-    const i=Math.floor(Math.random()*500)+1;
-    const j=Math.floor(Math.random()*500)+1;
-    
-    b.style.left=i+"px";
-    b.style.top=j+"px";
-})
+// const b = document.querySelector('button.no')
+// b.addEventListener('mouseover', () => {
+//     const i = Math.floor(Math.random() * 500) + 1;
+//     const j = Math.floor(Math.random() * 500) + 1;
 
-function f1(){
+//     b.style.left = i + "px";
+//     b.style.top = j + "px";
+// })
+
+var btn = document.querySelector("button.no"); 
+var position;
+
+btn.addEventListener("mouseover",()=> {
+    position ? (position = 0): (position = 150);
+    btn.style.transform = `translate(${position}px,0px)`;
+    btn.style.transition = "all 0.3s ease";
+});
+
+function f1() {
     alert("YAY Thank You (>_<)")
 }
